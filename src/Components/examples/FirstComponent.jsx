1a3474c8bd33e413fb9545ce29019ebb0cb1617e
divya prakash <d.prakash1506@gmail.com>
2021-10-16 12:42:28 +0530

    added firstComponent

---
+++ src/Components/examples/FirstComponent.jsx
@@ -0,0 +1,21 @@
+import React, {Component} from "react";
+
+export default class FirstComponent extends Component{
+    render(){
+    return(
+        <div >
+            Test FirstComponent
+        </div>
+    );
+    }
+}
+
+export class SecondComponent extends Component{
+    render(){
+        return(
+            <div>
+                SecondComponent
+            </div>
+        );
+    }
+}
